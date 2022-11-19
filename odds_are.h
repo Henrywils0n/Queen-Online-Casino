76f7ce197b470808e2c177c9ef8368570a7955aa
19mjm17 <19mjm17@queensu.ca>
2022-11-19 12:59:46 -0500

    header file

---
+++ odds_are.h
@@ -0,0 +1,32 @@
+#ifndef TEAM_23_MONTREAL_ODDS_ARE_H
+#define TEAM_23_MONTREAL_ODDS_ARE_H
+
+#include <string>
+using namespace std;
+
+//Class to handle invalid user input
+class BadUserInput {
+public:
+    BadJumbleException(const string&);
+    string& what();
+private:
+    string message;
+};
+
+class OddsAre {
+public:
+    int cardNumGen();
+    int bonusMultiplier();
+    int randomCardGen(); //Should return a card (but we do not have the card class yet)
+    int pointsEarned();
+    int searchList();
+
+private: //Declaring private variables for the JumblePuzzle class
+    int bet;
+    int score;
+    int countWin;
+    string playerGuess;
+    int cardLayout[];
+};
+
+#endif //TEAM_23_MONTREAL_ODDS_ARE_H
