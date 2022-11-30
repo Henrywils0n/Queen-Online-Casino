ea3bbe0dd9cfa38861d3c253e41b14cf464e76ba
Abbey <cameronab@cogeco.ca>
2022-11-30 14:56:48 -0500

    Syntax Changes

--- odds_are1.h
+++ odds_are1.h
@@ -15,10 +15,10 @@ private:
 
 class OddsAre {
 public:
-    void OddsAre(int);
+    void OddsAre(const int&);
     int numGen();
     int bonusMultiplier(int, int);
-    int randomNumGen(int); //generates a random number
+    void randomNumGen(int); //generates a random number
     //int searchList();  //Search card deck for player's guess
     void playerGuess(int, int);  //Feed in player guess and call searchList()
     int accountBalance;
@@ -28,9 +28,8 @@ private: //Declaring private variables for the JumblePuzzle class
     int score;
     int countWin; //Number of wins in a row for bonus multiplier
     int totalWin; //Overall wins, doesnt count wins in a row, just total number
-    string guess;
-    Card cards[];
-    Card cardGuess; //Players guess converted to a Card object
+    int guess;
+   
 };
 
 #endif //TEAM_23_MONTREAL_ODDS_ARE_H
