5edf2ad2f1fc87520b1b292b4082676987570689
Abbey <cameronab@cogeco.ca>
2022-11-30 17:04:06 -0500

    Fixed Issue with outputting correct balance amount

--- odds_are1.cpp
+++ odds_are1.cpp
@@ -8,7 +8,6 @@ BadUserInput::BadUserInput(const string& message) : message(message) {}
         }
 
 OddsAre::OddsAre(int accountBalance){
-    accountBalance = accountBalance;
     numGen(accountBalance); //Calls first function to get input regarding game
 }
 
@@ -16,26 +15,30 @@ OddsAre::OddsAre(int accountBalance){
 int OddsAre::numGen(int accountBalance){
     int numsToGuess;
     int bet;
-    cout << "How much would you like to bet ";
+    cout << "Please enter your bet amount:";
     cin >> bet;
-    if( bet > accountBalance){
-        throw BadUserInput ("Invalid bet amount");
+    if (bet > accountBalance){
+        throw BadUserInput ("Invalid bet amount!");
     }
-    cout << "How many numbers would you like to guess from? "; 
+    else if (cin.fail()) {
+        cout << "Error";
+        //throw BadUserInput("Please enter a number!");
+    }
+    cout << "Please enter how many numbers would you like to guess from: ";
     cin >> numsToGuess;
     //create cardsToGuess number of Card objects and add to a list
-    randomNumGen(numsToGuess);
+    randomNumGen(numsToGuess, accountBalance, bet);
 }
 
-void OddsAre::randomNumGen(int numsToGuess) {
+void OddsAre::randomNumGen(int numsToGuess, int accountBal, int betAmt) {
     int ans;
     ans = rand()%numsToGuess + 1;
-    playerGuess(ans, numsToGuess);
+    playerGuess(ans, numsToGuess, accountBal, betAmt);
     //return ans;
 } //generates a random number
 
-void OddsAre::playerGuess(int ans, int numsToGuess){
-    cout << "Guess a number within the range ";
+void OddsAre::playerGuess(int ans, int numsToGuess, int accountBal, int betAmt){
+    cout << "Guess a number within the range:";
     cin >> guess;
     int win = 0;
     if (guess > numsToGuess || guess < 0){
@@ -44,10 +47,11 @@ void OddsAre::playerGuess(int ans, int numsToGuess){
         if(ans == guess){
             win++;
             cout << "You win! \n";
-            bonusMultiplier(win, bet, accountBalance);
+            bonusMultiplier(win, bet, accountBal, betAmt);
         }else{
+            win = 0;
             cout << "You lost! \n";
-            bonusMultiplier(0, bet, accountBalance);
+            bonusMultiplier(win, bet, accountBal, betAmt);
             
         }
     }
@@ -55,24 +59,26 @@ void OddsAre::playerGuess(int ans, int numsToGuess){
 
 //If player has multiple wins in a row, multiply their score by the bonus points 
 //and return final score.
-int OddsAre::bonusMultiplier(int countWin, int bet, int balanceAmount) {
+int OddsAre::bonusMultiplier(int countWin, int bet, int accountBal, int betAmt) {
     int bonusAmount;
     int updateBalance;
     if (countWin == 1) {
         bonusAmount = 2 * countWin;
-        updateBalance = bonusAmount * bet;
+        updateBalance = bonusAmount * betAmt;
+        accountBal = updateBalance + accountBal;
     } else if (countWin == 2) {
         bonusAmount = 4 * countWin;
-        updateBalance = bonusAmount * bet;
+        updateBalance = bonusAmount * betAmt;
+        accountBal = updateBalance + accountBal;
     } else if (countWin > 2) {
         bonusAmount = 10 * countWin;
-        updateBalance = bonusAmount * bet;
+        updateBalance = bonusAmount * betAmt;
+        accountBal = updateBalance + accountBal;
     } else {
         bonusAmount = 1 * countWin;
-        updateBalance = balanceAmount - bet;
-        
+        accountBal = accountBal - betAmt;
     }
-    cout << "Your updated account balance: " << updateBalance;
+    cout << "Your updated account balance: " << accountBal;
     return updateBalance; //Player's credit balance after the game (shows if they won or lost)
 }
 
--- odds_are1.h
+++ odds_are1.h
@@ -17,17 +17,13 @@ class OddsAre {
 public:
     OddsAre(int);
     int numGen(int);
-    int bonusMultiplier(int, int, int);
-    void randomNumGen(int); //generates a random number
+    int bonusMultiplier(int, int, int, int);
+    void randomNumGen(int, int,int); //generates a random number
     //int searchList();  //Search card deck for player's guess
-    void playerGuess(int, int);  //Feed in player guess and call searchList()
-    int accountBalance;
+    void playerGuess(int, int, int, int);  //Feed in player guess and call searchList()
 
 private: //Declaring private variables for the JumblePuzzle class
     int bet;
-    int score;
-    int countWin; //Number of wins in a row for bonus multiplier
-    int totalWin; //Overall wins, doesnt count wins in a row, just total number
     int guess;
    
 };
