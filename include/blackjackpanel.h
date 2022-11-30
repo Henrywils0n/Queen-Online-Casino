e5f4eda577dc23c80f8b9ffe70cbe47126a70dfb
Victor <18vg5@queensu.ca>
2022-11-30 15:04:52 -0500

    Panels for player and dealer hand set up

--- include/blackjackpanel.h
+++ include/blackjackpanel.h
@@ -13,22 +13,35 @@ private:
 	*/
 	wxBoxSizer* gameSizer;
 
+	wxBoxSizer* dealerSizer;
+	wxBoxSizer* playerSizer;
+
 	/**
 	* Options are held in sizer at the bottom of the gameSizer
 	*/
 	wxBoxSizer* buttonSizer;
 
+	wxBoxSizer* betSizer;
+
 	/**
 	* For now the game is run through text
 	*/
 	wxTextCtrl* tempText;
+	wxTextCtrl* tempText2;
 
 	wxButton* dealButton;
+	wxButton* betOneButton;
+	wxButton* betFiveButton;
+	wxButton* betTwentyFiveButton;
+
 
 	/**
 	* When Dealbutton is pressed game begins. deal button and betthing options to be detroyed 
 	*/
 	void onDeal(wxCommandEvent& event);
+	void onBetOne(wxCommandEvent& event);
+	void onBetFive(wxCommandEvent& event);
+	void onBetTwentyFive(wxCommandEvent& event);
 
   DECLARE_EVENT_TABLE()
 };
--- src/blackjackpanel.cpp
+++ src/blackjackpanel.cpp
@@ -2,21 +2,39 @@
 
 BEGIN_EVENT_TABLE(BlackjackPanel, wxPanel)
 	EVT_BUTTON(ID_BJDEAL, BlackjackPanel::onDeal)
+    EVT_BUTTON(ID_BJONECHIP, BlackjackPanel::onBetOne)
+    EVT_BUTTON(ID_BJFIVECHIP, BlackjackPanel::onBetFive)
+    EVT_BUTTON(ID_BJTWENTYFIVECHIP, BlackjackPanel::onBetTwentyFive)
 END_EVENT_TABLE()
 
 BlackjackPanel::BlackjackPanel(GameFrame* par) : wxPanel(par) {  
 
     // Declare sizers
     gameSizer = new wxBoxSizer(wxVERTICAL);
+    dealerSizer = new wxBoxSizer(wxHORIZONTAL);
+    playerSizer = new wxBoxSizer(wxHORIZONTAL);
     buttonSizer = new wxBoxSizer(wxHORIZONTAL);
 
-    tempText = new wxTextCtrl(this, -1, "My text.", wxDefaultPosition, wxSize(100, 60), wxTE_MULTILINE);
+    tempText = new wxTextCtrl(this, -1, "Dealer Hand", wxDefaultPosition, wxSize(500, 100), wxTE_MULTILINE);
+    tempText2 = new wxTextCtrl(this, -1, "My Hand", wxDefaultPosition, wxSize(500, 100), wxTE_MULTILINE);
+
     dealButton = new wxButton(this, ID_BJDEAL, "DEAL");
+    betOneButton = new wxButton(this, ID_BJONECHIP, "1$ CHIP");
+    betFiveButton = new wxButton(this, ID_BJFIVECHIP, "5$ CHIP");
+    betTwentyFiveButton = new wxButton(this, ID_BJTWENTYFIVECHIP, "25$ CHIP");
 
     // 1 to stretch up and down; wxEXPAND to stretch sideways
-    gameSizer->Add(tempText, 1, wxEXPAND | wxALL, 10);
+    //gameSizer->Add(tempText, 1, wxEXPAND | wxALL, 10);
+    dealerSizer->Add(tempText, 0, wxEXPAND | wxALL, 10);
+    playerSizer->Add(tempText2, 0, wxEXPAND | wxALL, 10);
+
+    buttonSizer->Add(betOneButton, 0, wxALL, 10);
+    buttonSizer->Add(betFiveButton, 0, wxALL, 10);
+    buttonSizer->Add(betTwentyFiveButton, 0, wxALL, 10);
     buttonSizer->Add(dealButton, 0, wxALL, 10);
 
+    gameSizer->Add(dealerSizer, wxEXPAND | wxALL, wxALIGN_CENTER);
+    gameSizer->Add(playerSizer, wxEXPAND | wxALL, wxALIGN_CENTER);
     gameSizer->Add(buttonSizer, 0, wxALIGN_CENTER); 
 
     SetSizerAndFit(gameSizer);
@@ -24,4 +42,19 @@ BlackjackPanel::BlackjackPanel(GameFrame* par) : wxPanel(par) {
 
 void BlackjackPanel::onDeal(wxCommandEvent& WXUNUSED(event)) {
     this->dealButton->Destroy();
+    this->betOneButton->Destroy();
+    this->betFiveButton->Destroy();
+    this->betTwentyFiveButton->Destroy();
+}
+
+void BlackjackPanel::onBetOne(wxCommandEvent& WXUNUSED(event)) {
+
+}
+
+void BlackjackPanel::onBetFive(wxCommandEvent& WXUNUSED(event)) {
+
+}
+
+void BlackjackPanel::onBetTwentyFive(wxCommandEvent& WXUNUSED(event)) {
+
 }
