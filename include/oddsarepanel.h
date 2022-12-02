0ae9e4008d8b9daaa5b63c13b35e087961678f3d
ejrsilver <ejrsilver@gmail.com>
2022-12-02 16:28:05 -0500

    Created the GUI for Odds Are and added a score to the Menu Panel

--- include/oddsarepanel.h
+++ include/oddsarepanel.h
@@ -2,17 +2,29 @@
 #include "header.h"
 #include "gameframe.h"
 #include "imagebutton.h"
+#include "oddsare.h"
 
 class OddsArePanel : public wxPanel {
 public:
 	OddsArePanel(GameFrame* par);
 
-  wxStaticText* m_textCtrl;
+  wxStaticText* title_static;
+  wxStaticText* range_static;
+  wxStaticText* guess_static;
+  wxStaticText* bet_static;
+  wxTextCtrl* range_ctrl;
+  wxTextCtrl* guess_ctrl;
+  wxTextCtrl* bet_ctrl;
+  wxStaticText* result_static;
   ImageButton* exit_button;
+  wxButton* guess_button;
 
 private:
 	GameFrame* parent;
+  OddsAre* oa;
+
 	void onExitOddsAre(wxCommandEvent &WXUNUSED(event));
+  void onGuess(wxCommandEvent &WXUNUSED(event));
 	
 	DECLARE_EVENT_TABLE()
 };
--- src/oddsarepanel.cpp
+++ src/oddsarepanel.cpp
@@ -1,23 +1,49 @@
 #include "../include/oddsarepanel.h"
+#include <stdexcept>
 
 BEGIN_EVENT_TABLE(OddsArePanel, wxPanel)
   EVT_BUTTON(ID_OAQUIT, OddsArePanel::onExitOddsAre)
+  EVT_BUTTON(ID_OAGUESS, OddsArePanel::onGuess)
 END_EVENT_TABLE()
 
 OddsArePanel::OddsArePanel(GameFrame* par) : wxPanel(par) {
   parent = par;
+  oa = new OddsAre(parent->getBalance());
   wxString backpng = "../resources/back_button.png";
   
-  m_textCtrl = new wxStaticText(this, wxID_ANY, "ODDS ARE", wxDefaultPosition, wxSize(100, wxDefaultCoord));
+  title_static = new wxStaticText(this, wxID_ANY, "ODDS ARE", wxDefaultPosition, wxSize(100, wxDefaultCoord));
   exit_button  = new ImageButton(this, ID_OAQUIT,  "MENU", backpng, wxBITMAP_TYPE_PNG, 40, 40);
+  
+  range_static = new wxStaticText(this, wxID_ANY, "Enter an upper limit (>1):", wxDefaultPosition, wxSize(200, wxDefaultCoord));
+  guess_static= new wxStaticText(this, wxID_ANY, "Enter your guess:", wxDefaultPosition, wxSize(200, wxDefaultCoord));
+  bet_static = new wxStaticText(this, wxID_ANY, "Enter your bet:", wxDefaultPosition, wxSize(200, wxDefaultCoord));
+  
+  range_ctrl = new wxTextCtrl(this, wxID_ANY, " ", wxDefaultPosition, wxSize(200, wxDefaultCoord));
+  guess_ctrl = new wxTextCtrl(this, wxID_ANY, " ", wxDefaultPosition, wxSize(200, wxDefaultCoord));
+  bet_ctrl = new wxTextCtrl(this, wxID_ANY, " ", wxDefaultPosition, wxSize(200, wxDefaultCoord));
+  
+  guess_button = new wxButton(this, ID_OAGUESS, "PLAY");
   
   wxBoxSizer* menuBox = new wxBoxSizer(wxHORIZONTAL);
   wxBoxSizer* vertBox = new wxBoxSizer(wxVERTICAL);
-  
-  menuBox->Add(exit_button, 1, wxSHAPED);
-  menuBox->Add(m_textCtrl, 1, wxSTRETCH_NOT);
+  wxBoxSizer* gameBox = new wxBoxSizer(wxHORIZONTAL);
+  wxFlexGridSizer* inputBox = new wxFlexGridSizer(7, 1, 3, 3);
 
-  vertBox->Add(menuBox, 7, wxEXPAND);
+  menuBox->Add(exit_button, 1, wxSHAPED | wxSTRETCH_NOT);
+  menuBox->Add(title_static, 1, wxSTRETCH_NOT);
+  
+  inputBox->Add(range_static, wxEXPAND);
+  inputBox->Add(range_ctrl, wxEXPAND);
+  inputBox->Add(guess_static, wxEXPAND);
+  inputBox->Add(guess_ctrl, wxEXPAND);
+  inputBox->Add(bet_static, wxEXPAND);
+  inputBox->Add(bet_ctrl, wxEXPAND);
+  
+  gameBox->Add(inputBox, wxEXPAND);
+  gameBox->Add(guess_button, wxEXPAND);
+
+  vertBox->Add(menuBox, 0, wxSTRETCH_NOT);
+  vertBox->Add(gameBox, 10, wxEXPAND);
   
   SetSizer(vertBox);
   vertBox->SetSizeHints(this);
@@ -25,4 +51,42 @@ OddsArePanel::OddsArePanel(GameFrame* par) : wxPanel(par) {
 
 void OddsArePanel::onExitOddsAre(wxCommandEvent &WXUNUSED(event)) {
   parent->returnToMenu();
+}
+
+void OddsArePanel::onGuess(wxCommandEvent &WXUNUSED(event)) {
+  wxString range = range_ctrl->GetLineText(0);
+  wxString guess = guess_ctrl->GetLineText(0);
+  wxString bet = bet_ctrl->GetLineText(0);
+  
+  try {
+    int rin = std::stoi(range.ToUTF8().data());
+    int gin = std::stoi(range.ToUTF8().data());
+    int bin = std::stoi(range.ToUTF8().data());
+    
+    int ret = oa->playGame(rin,gin,bin);
+    parent->setBalance(parent->getBalance() + ret);
+    if (ret > 0) {
+      wxArrayString array;
+      
+      array.Add("YOU WIN!!!");
+      array.Add("Your new balance is:");
+      array.Add(wxString::Format(wxT("%i"),parent->getBalance()));
+      (void)wxMessageBox(wxJoin(array, '\n'), "Result", wxICON_INFORMATION | wxOK );
+    }
+    else {
+      wxArrayString array;
+      
+      array.Add("You lose.");
+      array.Add("Your new balance is:");
+      array.Add(wxString::Format(wxT("%i"),parent->getBalance()));
+      (void)wxMessageBox(wxJoin(array, '\n'), "Result", wxICON_INFORMATION | wxOK );
+    }
+  }
+  catch (std::exception &e) {
+    wxArrayString array;
+    
+    array.Add("ERROR:");
+    array.Add(e.what());
+    (void)wxMessageBox(wxJoin(array, '\n'), "Error Message", wxICON_INFORMATION | wxOK );
+  }
 }
