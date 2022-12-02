7a77e42040c134ec5b4a28a5e93129edb79d9d48
Abbey <cameronab@cogeco.ca>
2022-12-02 13:10:47 -0500

    Update playGame()

--- odds_are1.cpp
+++ odds_are1.cpp
@@ -8,88 +8,69 @@ string& BadUserInput::what() {
 }
 
 OddsAre::OddsAre(int accountBalance){
-    numGen(accountBalance); //Calls first function to get input regarding game
+    //numGen(accountBalance); //Calls first function to get input regarding game
+    accountBalance = accountBalance;
+    int topRange = 0;  //Player guesses a number between 0 and topRange
+    int bet = 0;
+    int guess = 0;
 }
-//Asks user how many cards they want to guess, returns number
-int OddsAre::numGen(int accountBalance){
-    int numsToGuess;
-    int bet;
-    cout << "Please enter your bet amount:";
-    cin >> bet;
-    if (bet > accountBalance){
-        throw BadUserInput ("Invalid bet amount!");
-    }
-    else if (cin.fail()) {
-        throw BadUserInput("Please enter a number!");
-    }
-    //Asks uses for number range
-    cout << "Please enter how many numbers would you like to guess from: ";
-    cin >> numsToGuess;
-    if (numsToGuess == 1 || numsToGuess == 0) {
+
+//Plays the game
+int OddsAre::playGame(int accountBalance, int topRange, int guess, int bet){
+
+    if (topRange == 1 || topRange == 0) {
         throw BadUserInput("Please enter a number greater than one and zero.");
     }
     if (cin.fail()) {
         throw BadUserInput("Please enter a valid number.");
     }
-    randomNumGen(numsToGuess, accountBalance, bet);
-}
-
-//function generates a random number between 1 and the upper bound.
-void OddsAre::randomNumGen(int numsToGuess, int accountBal, int betAmt) {
-    int ans;
-    ans = rand()%numsToGuess + 1;
-    playerGuess(ans, numsToGuess, accountBal, betAmt);
-}
-
-//function asks users to guess a number
-void OddsAre::playerGuess(int ans, int numsToGuess, int accountBal, int betAmt){
-    cout << "Guess a number within the range:";
-    cin >> guess;
+// generates a random number between 1 and the upper bound.
+    int ans = rand()%topRange + 1;
+//asks users to guess a number
     int win = 0;
-    if (guess > numsToGuess || guess < 0){
+    if (guess > topRange || guess < 0){
         throw BadUserInput("Invalid Input");
     }else{
         if(ans == guess){
             win++;
             cout << "You win! \n";
-            bonusMultiplier(win, bet, accountBal, betAmt, numsToGuess);
+            bonusMultiplier(win, bet, accountBalance, topRange);
         }else{
             win = 0;
             cout << "You lost! \n";
-            bonusMultiplier(win, bet, accountBal, betAmt, numsToGuess);
+            bonusMultiplier(win, bet, accountBalance, topRange);
         }
     }
 }
-
 //Calculate the score
-int OddsAre::bonusMultiplier(int countWin, int bet, int accountBal, int betAmt, int numsToGuess){
+int OddsAre::bonusMultiplier(int countWin, int bet, int accountBalance, int topRange){
     int bonusAmount;
     int updateBalance;
     if(countWin >= 1){
-        if(numsToGuess == 2){
+        if(topRange == 2){
             bonusAmount = 2 * countWin;
-            updateBalance = bonusAmount * betAmt;
-            accountBal = updateBalance + accountBal;
-        }else if(2 < numsToGuess < 5){
+            updateBalance = bonusAmount * bet;
+            accountBalance = updateBalance + accountBalance;
+        }else if(2 < topRange < 5){
             bonusAmount = 5 * countWin;
-            updateBalance = bonusAmount * betAmt;
-            accountBal = updateBalance + accountBal;
-        }else if(5 < numsToGuess < 10){
+            updateBalance = bonusAmount * bet;
+            accountBalance = updateBalance + accountBalance;
+        }else if(5 < topRange < 10){
             bonusAmount = 7 * countWin;
-            updateBalance = bonusAmount * betAmt;
-            accountBal = updateBalance + accountBal;
-        }else if(10 < numsToGuess){
+            updateBalance = bonusAmount * bet;
+            accountBalance = updateBalance + accountBalance;
+        }else if(10 < topRange){
             bonusAmount = 10 * countWin;
-            updateBalance = bonusAmount * betAmt;
-            accountBal = updateBalance + accountBal;
+            updateBalance = bonusAmount * bet;
+            accountBalance = updateBalance + accountBalance;
         }else{
             ;
         }
     }else{
         bonusAmount = 1 * countWin;
-        accountBal = accountBal - betAmt;
+        accountBalance = accountBalance - bet;
     }
-    cout << "Your updated account balance: " << accountBal;
+    cout << "Your updated account balance: " << accountBalance;
     return updateBalance; //Player's credit balance after the game (shows if they won or lost)
 }
 
--- odds_are1.h
+++ odds_are1.h
@@ -16,16 +16,17 @@ private:
 class OddsAre {
 public:
     OddsAre(int);
-    int numGen(int);
-    int bonusMultiplier(int, int, int, int, int);
-    void randomNumGen(int, int,int); //generates a random number
-    //int searchList();  //Search card deck for player's guess
-    void playerGuess(int, int, int, int);  //Feed in player guess and call searchList()
+    int playGame(int, int, int, int);
+    
+    int accountBalance;
 
-private: //Declaring private variables for the JumblePuzzle class
+private: 
+    int bonusMultiplier(int, int, int, int);
+    
     int bet;
     int guess;
-   
+    int topRange;
+    
 };
 
 #endif //TEAM_23_MONTREAL_ODDS_ARE_H
